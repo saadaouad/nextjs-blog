8a250c26a1df52296d59c346bcb1f158f9055ed9
saadouad <aouadsaad05@gmail.com>
2022-04-01 13:11:12 +0000

    first commit

---
+++ components/date.tsx
@@ -0,0 +1,8 @@
+import { format, parseISO } from 'date-fns';
+
+function Date({ dateString }: { dateString: string }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+};
+
+export default Date;
---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
+
+function Home({ allPostsData }: { allPostsData: { date: string; title: string; id: string }[] }) {
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I am Saad Aouad thirty years old, born and raised in Casablanca
+          Morocco. I'm a Front-end Engineer with more than 5 years of experience
+          in web development. I help companies grow and scale through software
+          engineering practices so they can react to changes faster. Technically
+          astute and versatile with ability to quickly learn, master, and
+          leverage new technologies to meet business needs. I like solving real
+          challenging problems and sharing experiences with other people. Fluent
+          in English, French and Arabic with excellent communication and
+          interpersonal skills. Strong work ethic, analytical mindset and
+          technical skills to drive product success and process efficiency.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+export default Home;
